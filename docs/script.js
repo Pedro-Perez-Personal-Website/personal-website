1821784a610e761261d96fadf70c7a5591641a9e
Pedro Perez <pipr96@gmail.com>
2023-03-31 21:23:43 -0700

    add the javascript alert to the about page

---
+++ docs/script.js
@@ -0,0 +1,6 @@
+//verify connection---------|
+console.log("It works")
+//send alert with message---|
+function start(){
+    alert("Felicidades activaste un area secreta creada con JavaScript, mueve el pointer sobre la pagina para interactuar con ella. ")
+}
