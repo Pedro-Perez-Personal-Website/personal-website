f169710dd137fb89c98e7994e6119d6dd3fb4824
Pedro Perez <pipr96@gmail.com>
2023-04-05 15:28:55 -0700

    add javascript to portfolio and links to portfolio pages

--- docs/script.js
+++ docs/script.js
@@ -1,6 +1,25 @@
 //verify connection---------|
 console.log("It works")
 //send alert with message---|
+
 function start(){
-    alert("Felicidades activaste un area secreta creada con JavaScript, mueve el pointer sobre la pagina para interactuar con ella. ")
+    console.log("press logo")
+    let key = "10-40-19"
+    if (key == "10-40-19"){
+        alert("Felicidades Activaste un area secreta.Mueve el mouse para interactuar con el UI")
+    }else{
+        alert("Something Else")
+    }
+}
+
+//Prompt Method
+function key(){
+    console.log("Secure the Vault press")
+    let key = prompt("key","**-**-**")
+    if (key == "10-40-19"){
+        window.open("https://pivanrios.github.io/CourseWork/")
+
+    }else{
+        alert("Keep Looking")
+    }
 }
